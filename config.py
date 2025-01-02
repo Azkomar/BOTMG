2e7ff61b1408f5236cacefc2d948ff20e7c06585
Azkomar <guillaume.martin110@gmail.com>
2025-01-02 10:13:00 +0100

    add .env variables, modify .gitignore and config.py

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
-SECRET_KEY = '29e50e4c8cdc3cc45ecc9f2553331fa1c12f5bec86ee51c0daa7571022c5960e'
+import os
+from dotenv import find_dotenv, load_doatenv
+
+# find .env automatically
+dotenvPath = find_dotenv()
+
+# load up the entries as environment variables
+load_doatenv(dotenvPath)
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # Configuration Flask-Mail
 MAIL_SERVER = 'smtp.gmail.com'  # Serveur SMTP de Gmail
@@ -6,6 +15,6 @@ MAIL_PORT = 587  # Port SMTP
 MAIL_USE_TLS = True  # Utiliser TLS (chiffrement)
 MAIL_USE_SSL = False  # SSL désactivé
 MAIL_USERNAME = 'azkorotmg@gmail.com'  # Votre adresse Gmail
-MAIL_PASSWORD = 'doxkybuciotltxhg'  # Mot de passe ou mot de passe d'application Gmail
+MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
 MAIL_DEFAULT_SENDER = ('ROTMGCollector', 'azkoROTMG@gmail.com')  # Nom et e-mail de l'expéditeur
 MAIL_MAX_EMAILS = 5  # Nombre maximal d'e-mails à envoyer en une fois (facultatif)
